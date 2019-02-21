41186caec767c7905099ef86685386ee92ca6d8f
fr3d100 <f.bonnand@gmail.com>
2019-02-21 20:18:49 +0100

    Script 5 ok

---
+++ script_7.js
@@ -0,0 +1,7 @@
+const books = [
+  { title: 'Gatsby le magnifique', id: 133712, rented: 9 },
+  { title: 'A la recherche du temps,perdu', id: 237634, rented: 2 },
+  { title: 'Orgueil & Préjugés', id: 873495, rented: 6 },
+  { title: 'Les frères Karamazov', id: 450911, rented: 5 },
+  { title: 'Guerre et Paix', id: 748147, rented: 19 }
+];
