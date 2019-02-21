4d5eb7e9808a010ff51ac663ed6a6dac61d50da2
fr3d100 <f.bonnand@gmail.com>
2019-02-21 17:58:16 +0100

    Commit Exo 1

---
+++ script1.js
@@ -0,0 +1,10 @@
+
+//2.1.1
+console.log("2.1.1 : ")
+console.log("Bonjour monde");
+
+// 2.2.2
+console.log("2.1.2 : ")
+let name = prompt("Hello, quel est ton nom ?")
+let helloSentence = `Bonjour, ${name} !`
+console.log(helloSentence)
