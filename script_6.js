a5b3ae40764f0a2c2806b34a9414ff91713b043d
fr3d100 <f.bonnand@gmail.com>
2019-02-21 22:58:59 +0100

    script 6 ok

--- script_6.js
+++ script_6.js
@@ -12,3 +12,39 @@ const entrepreneurs = [
   { first: 'Marc', last: 'Andreessen', year: 1971 },
   { first: 'Peter', last: 'Thiel', year: 1967 }
 ];
+
+
+//Constitution d'un  hash des entrepreneurs néés dans les années 70:
+const entrepreneurs70s = []
+for(i=0; i< entrepreneurs.length; i ++){
+  if(entrepreneurs[i].year > 1969 && entrepreneurs[i].year < 1980 ){
+    entrepreneurs70s.push(entrepreneurs[i])
+  }
+}
+//Afichage des résultats
+console.log("Tableau des entrepreneurs nés dans les années 70 :");
+console.log(entrepreneurs70s)
+
+// Affichage de la combinaison "prénom nom" de chaque entrepreneurs
+console.log("Tableau des noms complets de tous ces brillants hommes :");
+let fullNames = []
+for(i=0; i< entrepreneurs.length; i ++){
+  fullNames.push(entrepreneurs[i].first +" "+ entrepreneurs[i].last);
+}
+console.log(fullNames);
+
+//Calcul des âge à date de ces inventeurs :
+console.log("Voici l'âge qu'auraient chacun des ces inventeurs en 2019 :");
+const currentYear = new Date().getFullYear();
+for(i=0; i< entrepreneurs.length; i ++){
+  console.log(entrepreneurs[i].first +" "+ entrepreneurs[i].last+" : " + (currentYear - entrepreneurs[i].year ) + " ans");
+}
+
+//Trie des inventeurs par nom de famille :
+console.log("Liste des inventeurs triés alphabétiquement par nom de famille : ");
+sortedArray = entrepreneurs.sort(function(a,b){
+  if(a.last < b.last) {return -1;}
+  if(a.last > b.last) {return 1;}
+    return 0;
+})
+console.log(sortedArray);
